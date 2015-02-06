d918670fabe13d96b2faacc522465f8990f70605
A.D. <adotddot1123@gmail.com>
2015-02-06 18:29:43 +0100

    Create s1.py

---
+++ s1.py
@@ -0,0 +1,14 @@
+import utils
+
+def ch1():
+  '''Challenge 1 - Convert hex to base64'''
+  s = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+  return utils.hex2base64(s)
+  #>>> b'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
+  
+def ch2():
+  '''Challenge 2 - Fixed XOR
+  Write a function that takes two equal-length buffers and produces their XOR combination'''
+  s1 = "1c0111001f010100061a024b53535009181c"
+  s2 = "686974207468652062756c6c277320657965"
+  return utils.xor_hexstr(s1, s2)
