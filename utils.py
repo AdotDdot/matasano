d4ef3aa379b684025979843ed0615fad8eb6514d
A.D. <adotddot1123@gmail.com>
2015-02-06 18:42:07 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,5 +6,5 @@ def hex2base64(hexstr):
   return base64.b64encode(binascii.unhexlify(hexstr))
   
 def xor_hexstr(hexstr1, hexstr2):
-  '''Takes 2 hex strings and XORs them - returns hex string'''
-  return ''.join(["%02x"%(x[0]^x[1]) for x in zip(binascii.unhexlify(hexstr1), binascii.unhexlify(hexstr2))])
+  '''Takes 2 hex strings and XORs them - returns hex binary string'''
+  return ''.join(["%02x"%(x[0]^x[1]) for x in zip(binascii.unhexlify(hexstr1), binascii.unhexlify(hexstr2))]).encode("ascii")
