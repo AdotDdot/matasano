8735f0754a00df35a9e163815dade11473a9469c
A.D. <adotddot1123@gmail.com>
2015-02-06 18:20:05 +0100

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,10 @@
+import binascii
+import base64
+
+def hex2base64(hexstr):
+  '''Takes hex string - returns base64 encoded binary string'''
+  return base64.b64encode(binascii.unhexlify(hexstr))
+  
+def xor_hexstr(hexstr1, hexstr2):
+  '''Takes 2 hex strings and XORs them - returns hex string'''
+  return ''.join(["%02x"%(x[0]^x[1]) for x in zip(binascii.unhexlify(s1), binascii.unhexlify(s2))])
