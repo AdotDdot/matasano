37e55c7aa2e567befff67f8c3e080d78e0e6a53a
A.D. <adotddot1123@gmail.com>
2015-02-09 15:02:26 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,10 +2,6 @@ import binascii
 import base64
 import string
 
-eng_freqs = { 'a': 8.167, 'b': 1.492, 'c': 2.782, 'd': 4.253, 'e': 12.702, 'f': 2.228, 'g': 2.015,
-          'h': 6.094, 'i': 6.966, 'j': 0.153, 'k': 0.772, 'l': 4.025, 'm': 2.406, 'n': 6.749, 'o': 7.507,
-          'p': 1.929, 'q': 0.095, 'r': 5.987, 's': 6.327, 't': 9.056, 'u': 2.758, 'v': 0.978, 'w': 2.360, 'x': 0.150, 'y': 1.974, 'z': 0.074 }
-
 def hex2base64(hexstr):
   '''Takes hex string - returns base64 encoded binary string'''
   return base64.b64encode(binascii.unhexlify(hexstr))
@@ -15,36 +11,16 @@ def xor_hexstr(hexstr1, hexstr2):
   return ''.join(["%02x"%(x[0]^x[1]) for x in zip(binascii.unhexlify(hexstr1), binascii.unhexlify(hexstr2))]).encode("ascii")
   
 def bruteforce_single_char(hexstr):
-    '''Takes hex string - XORs it with all printable characters - returns dict char:plain-text-binary-string'''
-    u = {}
-    for i in range(32, 127):
-        key = binascii.hexlify((chr(i)*len(hexstr)).encode("ascii"))
-        u[chr(i)] = binascii.unhexlify(xor_hexstr(hexstr, key))
-    return u
+  '''Takes hex string - XORs it with all printable characters incl. extended ascii- returns all possible decryptions'''
+  u = []
+  for i in range(256):
+    key = ''.join([str(hex(i))[2:] for j in range(len(hexstr))]).encode("ascii")
+    u.append(binascii.unhexlify(xor_hexstr(hexstr, key)))
+  return u
     
-def is_legit_str(binstr):
-    '''Return true if string doesn't contain unnprintable characters and cointains spaces'''
-    if all([i >= 32 and i < 127 for i in binstr]) and 32 in binstr: return True
-    
-def calc_freq_distr(binstr):
-    '''Takes binary string - returns dict letter:freq-percent'''
-    freqs = {}
-    procstr = ''.join([chr(l).lower() for l in binstr if chr(l) in string.ascii_letters])
-    n = len(procstr)
-    for i in procstr:
-        if i not in freqs: freqs[i] = procstr.count(i)/n*100
-    return freqs
-  
-def process_freq(freqs): 
-    '''Takes frequency dict - returns variance calculated on deviations from English letter frequency'''
-    if not freqs: return -1
-    devs = [] 
-    for i in freqs:
-        curr = freqs[i]
-        eng = eng_freqs[i]
-        dev = abs(curr-eng)
-        devs.append(dev)
-    return sum([d**2 for d in devs])/len(devs)
-   
-    
+def calc_lett_ratio(binstr):
+  '''Takes hex string - returns letter||whitespace to non letter||withespace ratio'''
+  check = string.ascii_letters + ' '
+  lettstr = ''.join([chr(c) for c in binstr if chr(c) in check])
+  return len(lettstr)/len(binstr)
     
